43e2666c181ce14523b7b75a7e84d453cbed4d87
안민현 <alsgus2ek@naver.com>
2021-04-30 15:34:17 +0900

    회원가입 페이지

--- src/components/MemberJoinPage.vue
+++ src/components/MemberJoinPage.vue
@@ -1,23 +1,65 @@
 <template>
   <form>
     <div class="mb-3">
-      <label for="exampleInputEmail1" class="form-label">Email address</label>
+      <label for="exampleInputPassword1" class="form-label">Id</label>
+      <input
+        type="text"
+        class="form-control"
+        id="memberId"
+        v-model="member.memberId"
+      />
+    </div>
+
+    <div class="mb-3">
+      <label for="exampleInputPassword1" class="form-label">Password</label>
+      <input
+        type="password"
+        class="form-control"
+        id="memberPw"
+        v-model="member.memberPw"
+      />
+    </div>
+    <div class="mb-3">
+      <label for="memberName" class="form-label">Name</label>
+      <input
+        type="text"
+        class="form-control"
+        id="memberName"
+        v-model="member.memberName"
+      />
+    </div>
+
+    <div class="mb-3">
+      <label for="memberEmail" class="form-label">Email address</label>
       <input
         type="email"
         class="form-control"
         id="exampleInputEmail1"
         aria-describedby="emailHelp"
-        placeholder="user Email"
+        v-model="member.memberEamil"
       />
     </div>
     <div class="mb-3">
-      <label for="exampleInputPassword1" class="form-label">Password</label>
-      <input type="password" class="form-control" id="exampleInputPassword1" />
+      <label for="exampleInputPassword1" class="form-label"> Gender</label>
     </div>
-    <div class="mb-3 form-check">
-      <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-      <label class="form-check-label" for="exampleCheck1">Check me out</label>
+    <div class="mb-3">
+      <label for="exampleInputPassword1" class="form-label"> Male</label>
+      <input
+        type="radio"
+        class="form-control"
+        value="Male"
+        v-model="member.memberGender"
+        checked
+      />
+      <label for="exampleInputPassword1" class="form-label"> Female</label>
+      <input
+        type="radio"
+        class="form-control"
+        value="Female"
+        v-model="member.memberGender"
+      />
     </div>
+
     <button type="submit" class="btn btn-primary" @click="onSubmit()">
       Submit
     </button>
@@ -28,16 +70,38 @@
 </template>
 
 <script>
+import axios from "axios";
 export default {
-  data: function () {
-    return {};
+  data() {
+    return {
+      member: {
+        memberEmail: this.memberEmail,
+        memberId: this.memberId,
+        memberPw: this.memberPw,
+        memberName: this.memberName,
+        memberGender: this.memberGender,
+      },
+    };
   },
   methods: {
     onReset() {
-      console.log("Reset");
+      (this.memberId = ""),
+        (this.memberEmail = ""),
+        (this.memberPw = ""),
+        (this.memberName = ""),
+        (this.memberGender = null);
     },
     onSubmit() {
-      console.log("Submit");
+      axios
+        .post("http://localhost:8080/member", this.member)
+        .then((res) => {
+          console.log(res.data);
+          this.onReset();
+        })
+        .catch((e) => {
+          alert("회원가입 실패");
+          console.log(e);
+        });
     },
   },
 };
